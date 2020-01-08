8f6940270bc4161783ae10c7048eb9e00fcb8d26
shenke <690329169@qq.com>
2020-01-08 21:41:45 +0800

    chart

--- pages/my/my.js
+++ pages/my/my.js
@@ -1,14 +1,47 @@
 //my.js 仿照 index.js
 //获取应用实例
 const app = getApp()
+var wxCharts = require('../../utils/wxcharts.js');
+
+var ringChart = null;
+var columnChart = null;
+var chartData = {
+  main: {
+    title: '总成交量',
+    data: [15, 20, 45, 37],
+    categories: ['2012', '2013', '2014', '2015']
+  },
+  sub: [{
+    title: '2012年度成交量',
+    data: [70, 40, 65, 100, 34, 18],
+    categories: ['1', '2', '3', '4', '5', '6']
+  }, {
+    title: '2013年度成交量',
+    data: [55, 30, 45, 36, 56, 13],
+    categories: ['1', '2', '3', '4', '5', '6']
+  }, {
+    title: '2014年度成交量',
+    data: [76, 45, 32, 74, 54, 35],
+    categories: ['1', '2', '3', '4', '5', '6']
+  }, {
+    title: '2015年度成交量',
+    data: [76, 54, 23, 12, 45, 65],
+    categories: ['1', '2', '3', '4', '5', '6']
+  }]
+};
+
 
 Page({
   data: {
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    //chartTitle: '总成交量',
+    percent: 30,//进度条
+    isMainChartDisplay: true
   },
+ 
   //事件处理函数
   bindViewTap: function () {
     wx.navigateTo({
@@ -56,4 +89,150 @@ Page({
      url: "../myinfor/myinfor",
     })
   },
+
+//环形图
+  touchHandler: function (e) {
+    console.log(ringChart.getCurrentDataIndex(e));
+  },
+  updateData: function () {
+    ringChart.updateData({
+      title: {
+        name: '11%'
+      },
+      subtitle: {
+        color: '#ff0000'
+      }
+    });
+  },
+//柱状图
+  backToMainChart: function () {
+    this.setData({
+      chartTitle: chartData.main.title,
+      isMainChartDisplay: true
+    });
+    columnChart.updateData({
+      categories: chartData.main.categories,
+      series: [{
+        name: '成交量',
+        data: chartData.main.data,
+        format: function (val, name) {
+          return val.toFixed(2) + '万';
+        }
+      }]
+    });
+  },
+  touchHandler: function (e) {
+    var index = columnChart.getCurrentDataIndex(e);
+    if (index > -1 && index < chartData.sub.length && this.data.isMainChartDisplay) {
+      this.setData({
+        chartTitle: chartData.sub[index].title,
+        isMainChartDisplay: false
+      });
+      columnChart.updateData({
+        categories: chartData.sub[index].categories,
+        series: [{
+          name: '成交量',
+          data: chartData.sub[index].data,
+          format: function (val, name) {
+            return val.toFixed(2) + '万';
+          }
+        }]
+      });
+
+    }
+  },
+  onReady: function (e) {
+    var windowWidth = 320;
+    try {
+      var res = wx.getSystemInfoSync();
+      windowWidth = res.windowWidth;
+    } catch (e) {
+      console.error('getSystemInfoSync failed!');
+    }
+
+    ringChart = new wxCharts({
+      animation: true,
+      canvasId: 'ringCanvas',
+      type: 'ring',
+      extra: {
+        ringWidth: 25,
+        pie: {
+          offsetAngle: -45
+        }
+      },
+      title: {
+        name: '11%',
+        color: '#7cb5ec',
+        fontSize: 25
+      },
+      subtitle: {
+        name: '无抑郁',
+        color: '#666666',
+        fontSize: 15
+      },
+      series: [{
+        name: '成交量1',
+        data: 11,
+        stroke: false
+      }, {
+        name: '成交量2',
+        data: 89,
+        stroke: false,
+          color:'#EAEAEA'
+      }],
+      disablePieStroke: true,
+      width: windowWidth,
+      height: 200,
+      dataLabel: false,
+      legend: false,
+      background: '#f5f5f5',
+      padding: 0
+    });
+    ringChart.addEventListener('renderComplete', () => {
+      console.log('renderComplete');
+    });
+    setTimeout(() => {
+      ringChart.stopAnimation();
+    }, 500);
+
+
+    columnChart = new wxCharts({
+      canvasId: 'columnCanvas',
+      type: 'column',
+      animation: true,
+      categories: chartData.main.categories,
+      series: [{
+        name: '成交量',
+        data: chartData.main.data,
+        format: function (val, name) {
+          return val.toFixed(2) + '万';
+        }
+      }],
+      yAxis: {
+        format: function (val) {
+          return val + '万';
+        },
+        title: 'hello',
+        min: 0
+      },
+      xAxis: {
+        disableGrid: false,
+        type: 'calibration'
+      },
+      extra: {
+        column: {
+          width: 15
+        }
+      },
+      width: windowWidth,
+      height: 200,
+    });
+  },
+  progress: function () {
+    let that = this;
+    let percent = xxx; //获取percent
+    that.setData({
+      percent: percent
+    })
+  }
 })
