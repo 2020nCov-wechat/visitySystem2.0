a68ee474c2787e748061dbf964b642046f401838
shenkecoco <824459715@qq.com>
2020-01-30 20:53:49 +0800

    userinfo修改

--- pages/que/userinfo/userinfo.js
+++ pages/que/userinfo/userinfo.js
@@ -16,6 +16,10 @@ Page({
     married: '',
     education: '',
     bottom: false,
+    nextBtnText: "下一题",
+    questionHadAns: 0,
+    questionShowIndex: 0,
+    questionNum: 5,
     nationOption: ["汉族",
       "蒙古族",
       "藏族",
@@ -71,7 +75,133 @@ Page({
       "京族",
       "独龙族",
       "赫哲族",
-      "珞巴族"]
+      "珞巴族"],
+    questionShow: {
+      "type": "1",
+      "question": "2. 您的性别？",
+      "answers": [{
+        "answer": "男",
+        "value": 0
+      },
+      {
+        "answer": "女",
+        "value": 1
+      },
+      ]
+    },
+    questions: [{
+      "type": "1",
+      "question": "2. 您的性别？",
+      "answers": [{
+        "answer": "男",
+        "value": 0
+      },
+      {
+        "answer": "女",
+        "value": 1
+      },
+      ]
+    },
+    {
+      "type": "5",
+      "question": "3. 您的民族",
+      "answers": [{
+        "answer": "完全不会",
+        "value": 0
+      },
+      {
+        "answer": "好几天",
+        "value": 1
+      },
+      {
+        "answer": "一半以上的天数",
+        "value": 2
+      },
+      {
+        "answer": "几乎每天",
+        "value": 3
+      },
+      ]
+    },
+    {
+      "type": "1",
+      "question": "4. 您的年龄",
+      "answers": [{
+        "answer": "18~25",
+        "value": 0
+      },
+      {
+        "answer": "26~30",
+        "value": 1
+      },
+      {
+        "answer": "31~40",
+        "value": 2
+      },
+      {
+        "answer": "41~50",
+        "value": 3
+      },
+      {
+        "answer": "51~60",
+        "value": 4
+      },
+      {
+        "answer": "60以上",
+        "value": 5
+      },
+    
+      ]
+    },
+    {
+        "type": "1",
+        "question": "5. 婚姻状况",
+        "answers": [{
+          "answer": "未婚",
+          "value": 0
+        },
+        {
+          "answer": "已婚",
+          "value": 1
+        },
+        {
+          "answer": "丧偶",
+          "value": 2
+        },
+        {
+          "answer": "离异",
+          "value": 3
+        },
+        ]
+      },
+      {
+        "type": "1",
+        "question": "6. 学历",
+        "answers": [{
+          "answer": "初中及以下",
+          "value": 0
+        },
+        {
+          "answer": "高中/中专",
+          "value": 1
+        },
+        {
+          "answer": "本科/大专",
+          "value": 2
+        },
+        {
+          "answer": "硕士",
+          "value": 3
+        },
+        {
+          "answer": "博士",
+          "value": 4
+        },
+        ]
+      }
+    ],
+    answers: []
+    
   },
 
   onLoad: function () {
@@ -144,30 +274,167 @@ Page({
       [type]: !this.data[type]
     });
   },
-  //性别
-  onChangeSex: function (event) {
+
+  onChange(event) {
+
+    console.log(this.data.questionHadAns + '  ' + this.data.questionShowIndex + '  ' + (this.data.questionNum - 1))
+    if (this.data.questionHadAns >= this.data.questionNum) {
+      console.log("回答完了")
+      var ansNew = this.data.answers;
+      ansNew[this.data.questionShowIndex] = event.detail
+      this.setData({
+        answers: ansNew
+      })
+    } else {
+      console.log("还没回答完")
+      this.data.answers.push(event.detail)
+    }
     this.setData({
-      sex: event.detail
+      radio: event.detail
     })
+    console.log(this.data.answers)
+    var that = this
+    var time = setTimeout(function () {
+      if (that.data.questionShowIndex == that.data.questionNum - 2) {
+        that.setData({
+          nextBtnText: "完成"
+        })
+      }
+      if (that.data.questionShowIndex == that.data.questionNum - 1) {
+        //回答完毕
+        console.log("finish")
+        that.setData({
+          questionHadAns: that.data.questionHadAns + 1,
+          nextBtnText: "完成"
+        })
+      } else {
+        console.log(that.data.radio)
+        //如果后面的回答过了，就显示后面的题目
+        var ra = ''
+        if (that.data.questionHadAns > that.data.questionShowIndex) {
+          ra = that.data.answers[that.data.questionShowIndex + 1]
+          console.log("ra::" + ra)
+        }
+        that.setData({
+          questionShow: that.data.questions[that.data.questionShowIndex + 1],
+          questionShowIndex: that.data.questionShowIndex + 1,
+          questionHadAns: that.data.questionHadAns + 1,
+          radio: ra
+        });
+      }
+    }, 500)
+    // if (this.data.questionShowIndex == this.data.questionNum - 1) {
+    //   //回答完毕
+    //   console.log("finish")
+    // } else {
+    //   console.log(this.data.radio)
+    //   this.setData({
+    //     questionShow: this.data.questions[this.data.questionShowIndex + 1],
+    //     questionShowIndex: this.data.questionShowIndex + 1,
+    //     radio: ''
+    //   });
+    // }
   },
-  //年龄
-  onChangeAge: function (event) {
-    this.setData({
-      age: event.detail
-    })
+  clickBtnLast() {
+    if (this.data.questionShowIndex != 0) {
+      console.log(this.data.questions[this.data.questionShowIndex])
+      this.setData({
+        nextBtnText: "下一题",
+        questionShow: this.data.questions[this.data.questionShowIndex - 1],
+        questionShowIndex: this.data.questionShowIndex - 1,
+        radio: this.data.answers[this.data.questionShowIndex - 1]
+      });
+    } else {
+      console.log("first one")
+    }
   },
-  //婚姻
-  onChangeMarried: function (event) {
-    this.setData({
-      married: event.detail
-    })
+  clickBtnNext() {
+    console.log(this.data.questionHadAns + '  ' + this.data.questionShowIndex + '     ' + (this.data.questionNum - 1))
+    if (this.data.questionHadAns <= this.data.questionShowIndex) {
+      console.log("还没答完当前题目")
+      Toast.fail('请先回答当前问题');
+    } else {
+      if (this.data.questionShowIndex == this.data.questionNum - 1) {
+        //回答完毕
+        console.log("last one")
+        this.setData({
+          nextBtnText: "完成"
+        })
+        this.sendAnswer(this.data.curScaleIndex)
+      } else {
+        console.log(this.data.questionShowIndex)
+        if (this.data.questionShowIndex == this.data.questionNum - 2) {
+          //是最后一道题
+          if (this.data.questionHadAns <= this.data.questionShowIndex + 1) {
+            //下一题还没回答
+            this.setData({
+              nextBtnText: "完成",
+              questionShow: this.data.questions[this.data.questionShowIndex + 1],
+              radio: '',
+              questionShowIndex: this.data.questionShowIndex + 1,
+            })
+          } else {
+            this.setData({
+              nextBtnText: "完成",
+              questionShow: this.data.questions[this.data.questionShowIndex + 1],
+              radio: this.data.answers[this.data.questionShowIndex + 1],
+              questionShowIndex: this.data.questionShowIndex + 1,
+            })
+          }
+
+        } else {
+          //不是最后一道题
+          if (this.data.questionHadAns <= this.data.questionShowIndex + 1) {
+            //下一题还没回答
+            this.setData({
+
+              questionShow: this.data.questions[this.data.questionShowIndex + 1],
+              radio: '',
+              questionShowIndex: this.data.questionShowIndex + 1,
+            })
+          } else {
+            this.setData({
+              questionShow: this.data.questions[this.data.questionShowIndex + 1],
+              radio: this.data.answers[this.data.questionShowIndex + 1],
+              questionShowIndex: this.data.questionShowIndex + 1,
+              nextBtnText: "完成"
+            });
+          }
+
+        }
+      }
+
+
+
+    }
   },
-  //学历
-  onChangeEducation: function (event) {
-    this.setData({
-      education: event.detail
-    })
-  },
+
+
+
+  // //性别
+  // onChangeSex: function (event) {
+  //   this.setData({
+  //     sex: event.detail
+  //   })
+  // },
+  // //年龄
+  // onChangeAge: function (event) {
+  //   this.setData({
+  //     age: event.detail
+  //   })
+  // },
+  // //婚姻
+  // onChangeMarried: function (event) {
+  //   this.setData({
+  //     married: event.detail
+  //   })
+  // },
+  // //学历
+  // onChangeEducation: function (event) {
+  //   this.setData({
+  //     education: event.detail
+  //   })
+  // },
 
   //民族显示
   showBottom: function () {
